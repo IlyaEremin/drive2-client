d3f25cc6fa6098760dd282fe9dca46ce1ee624ba
Ilya Eremin <partissanei@gmail.com>
2016-11-21 00:52:05 +0300

    improve login screen

--- app/src/main/java/com/example/ereminilya/drive2_ok/login/LoginScreen.java
+++ app/src/main/java/com/example/ereminilya/drive2_ok/login/LoginScreen.java
@@ -1,22 +1,22 @@
 package com.example.ereminilya.drive2_ok.login;
 
-import android.support.annotation.NonNull;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.bluelinelabs.conductor.Controller;
+import com.bluelinelabs.conductor.RouterTransaction;
+import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler;
 import com.example.ereminilya.drive2_ok.R;
+import com.example.ereminilya.drive2_ok.UserInteractor;
 import com.example.ereminilya.drive2_ok.login.models.LoginBody;
-import com.example.ereminilya.drive2_ok.utils.Deps;
+import com.example.ereminilya.drive2_ok.profile.ProfileScreen;
+import com.example.ereminilya.drive2_ok.utils.Rxs;
+import com.example.ereminilya.drive2_ok.utils.di.Injector;
+import com.example.ereminilya.drive2_ok.utils.ui.BaseController;
+import com.example.ereminilya.drive2_ok.utils.ui.ScreenParams;
+
+import javax.inject.Inject;
 
 import butterknife.Bind;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 import static com.example.ereminilya.drive2_ok.utils.TextUtils.textOf;
 
@@ -24,25 +24,31 @@ import static com.example.ereminilya.drive2_ok.utils.TextUtils.textOf;
  * Created by ereminilya on 15/11/16.
  */
 
-public class LoginScreen extends Controller {
+public class LoginScreen extends BaseController {
+
+    @Inject UserInteractor userInteractor;
 
     @Bind(R.id.et_login)    EditText uiLoginEt;
     @Bind(R.id.et_password) EditText uiPasswordEt;
 
-    @NonNull @Override
-    protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
-        View view = inflater.inflate(R.layout.screen_login, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+    @Override protected ScreenParams getScreenParams() {
+        return new ScreenParams(R.layout.screen_login).injectDependency();
+    }
+
+    @Override protected void inject() {
+        Injector.inject(this);
     }
 
     @OnClick(R.id.enter) void onEnterClick() {
         LoginBody loginBody = new LoginBody(textOf(uiLoginEt), textOf(uiPasswordEt));
-        Deps.providesApi().login(loginBody)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+        userInteractor.login(loginBody)
+            .compose(Rxs.doInBackgroundDeliverToUI())
             .subscribe(result -> {
-                Toast.makeText(getActivity(), "Successfully logged in", Toast.LENGTH_SHORT).show();
+                getRouter().pushController(RouterTransaction
+                    .with(new ProfileScreen())
+                    .pushChangeHandler(new HorizontalChangeHandler())
+                    .popChangeHandler(new HorizontalChangeHandler())
+                );
             }, error -> { });
     }
 }
