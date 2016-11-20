0d557279d53fc25b61fbd32462b471e17ecd653b
Ilya Eremin <partissanei@gmail.com>
2016-11-21 00:46:26 +0300

    set my own app implementation

---
+++ app/src/main/java/com/example/ereminilya/drive2_ok/App.java
@@ -0,0 +1,26 @@
+package com.example.ereminilya.drive2_ok;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+import com.example.ereminilya.drive2_ok.utils.di.DaggerSingletonsComponent;
+import com.example.ereminilya.drive2_ok.utils.di.Singletons;
+import com.example.ereminilya.drive2_ok.utils.di.SingletonsComponent;
+
+/**
+ * Created by ereminilya on 20/11/16.
+ */
+
+public class App extends Application {
+
+    @NonNull SingletonsComponent component;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        component = DaggerSingletonsComponent.builder().singletons(new Singletons(this)).build();
+    }
+
+    public SingletonsComponent getComponent() {
+        return component;
+    }
+}
