37a06d35b4778b03f637533ddc12b2e91c2f281e
Ilya Eremin <partissanei@gmail.com>
2017-01-23 01:15:27 +0300

    add context method to BaseController

--- app/src/main/java/com/example/ereminilya/drive2_ok/utils/ui/BaseController.java
+++ app/src/main/java/com/example/ereminilya/drive2_ok/utils/ui/BaseController.java
@@ -1,5 +1,6 @@
 package com.example.ereminilya.drive2_ok.utils.ui;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,4 +35,8 @@ public abstract class BaseController extends Controller {
     }
 
     protected abstract ScreenParams getScreenParams();
+
+    protected Context context() {
+        return getActivity();
+    }
 }
