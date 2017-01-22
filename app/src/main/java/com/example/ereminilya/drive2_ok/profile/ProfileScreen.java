6ea47fc44c3dc40b88b7acc4a152c64e3de053b5
Ilya Eremin <partissanei@gmail.com>
2017-01-23 01:16:13 +0300

    show name, city and first car from list on profile screen

--- app/src/main/java/com/example/ereminilya/drive2_ok/profile/ProfileScreen.java
+++ app/src/main/java/com/example/ereminilya/drive2_ok/profile/ProfileScreen.java
@@ -1,13 +1,19 @@
 package com.example.ereminilya.drive2_ok.profile;
 
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.ereminilya.drive2_ok.R;
 import com.example.ereminilya.drive2_ok.UserInteractor;
+import com.example.ereminilya.drive2_ok.login.models.User;
+import com.example.ereminilya.drive2_ok.models.Car;
 import com.example.ereminilya.drive2_ok.utils.di.Injector;
+import com.example.ereminilya.drive2_ok.utils.di.Lists;
 import com.example.ereminilya.drive2_ok.utils.ui.BaseController;
 import com.example.ereminilya.drive2_ok.utils.ui.ScreenParams;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -22,6 +28,9 @@ public class ProfileScreen extends BaseController {
     @Inject UserInteractor userInteractor;
 
     @Bind(R.id.avatar) ImageView uiAvatar;
+    @Bind(R.id.name)   TextView  uiName;
+    @Bind(R.id.city)   TextView  uiCity;
+    @Bind(R.id.car)    TextView  uiCar;
 
     @Override protected ScreenParams getScreenParams() {
         return new ScreenParams(R.layout.screen_profile).injectDependency();
@@ -32,6 +41,15 @@ public class ProfileScreen extends BaseController {
     }
 
     @Override protected void onViewInitialized() {
-        Glide.with(getActivity()).load(userInteractor.getAvatarUrl()).into(uiAvatar);
+        User user = userInteractor.getUser();
+        Glide.with(context()).load(user.getBestProfileImageUrl()).into(uiAvatar);
+        uiName.setText(user.getName());
+        uiCity.setText(user.getCity());
+        List<Car> userCars = userInteractor.getUserCars();
+        if (Lists.isEmpty(userCars)) {
+            uiCar.setText(R.string.profile_no_cars);
+        } else {
+            uiCar.setText(userCars.get(0).getName());
+        }
     }
 }
